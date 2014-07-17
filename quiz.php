418731f1caa7e350e4a923cf5a55755a6eef777a
Karthik M <karthikappu87@gmail.com>
2014-07-18 02:30:54 +0530

    Quiz changes

--- quiz.php
+++ quiz.php
@@ -4,116 +4,123 @@ include("database.php");
 extract($_POST);
 extract($_GET);
 extract($_SESSION);
-/*$rs=mysql_query("select * from mst_question where test_id=$tid",$cn) or die(mysql_error());
-if($_SESSION[qn]>mysql_num_rows($rs))
-{
-unset($_SESSION[qn]);
-exit;
-}*/
-if(isset($subid) && isset($testid))
-{
-$_SESSION["sid"]=$subid;
-$_SESSION["tid"]=$testid;
-header("location:quiz.php");
+
+if(isset($subid) && isset($testid)) {
+	$_SESSION["sid"] = $subid;
+	$_SESSION["tid"] = $testid;
+	
+	$query = "SELECT * FROM mst_useranswer WHERE sess_id='".session_id()."' AND test_id=".$testid;
+	$result = mysql_query($query, $cn) or die(mysql_error());
+	
+	if (mysql_num_rows($result) == 0) {
+		$query1 = "SELECT * FROM mst_question WHERE test_id=".$testid." ORDER BY RAND() LIMIT 25";
+		$result1 = mysql_query($query1, $cn) or die(mysql_error());
+		while($row = mysql_fetch_row($result1)) {
+			$query2 = "INSERT INTO mst_useranswer(sess_id, test_id, que_des, ans1, ans2, ans3, ans4, true_ans, your_ans) values ('".session_id()."', ".$testid.",'$row[2]','$row[3]','$row[4]','$row[5]', '$row[6]','$row[7]','')";
+			mysql_query($query2) or die(mysql_error());
+		}
+	}
+	$_SESSION["qn"] = 0;
+	$_SESSION["trueans"] = 0;
+	//echo "Sucessfull".$_SESSION["qn"];
+	header("location:quiz.php");
 }
-if(!isset($_SESSION["sid"]) || !isset($_SESSION["tid"]))
-{
+
+if(!isset($_SESSION["sid"]) || !isset($_SESSION["tid"])) {
+	//echo "Fiale case";
 	header("location: index.php");
 }
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<!DOCTYPE html>
 <html>
-<head>
-<title>Online Quiz</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<link href="quiz.css" rel="stylesheet" type="text/css">
-</head>
+	<head>
+		<title>Online Quiz</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<link href="quiz.css" rel="stylesheet" type="text/css">
+	</head>
 
 <body>
 <?php
 include("header.php");
-
 $submit = (isset($_POST["submit"]))?$_POST["submit"]:"";
 
-$query="select * from mst_question";
-
-$rs=mysql_query("select * from mst_question where test_id=$tid ORDER BY RAND() Limit 1" ,$cn) or die(mysql_error());
-
-if(!isset($_SESSION["qn"]))
-{
-	$_SESSION["qn"]=0;
-	mysql_query("delete from mst_useranswer where sess_id='" . session_id() ."'") or die(mysql_error());
-	$_SESSION["trueans"]=0;
+if(isset($_SESSION["qn"])) {
+	$query3 = "SELECT * FROM mst_useranswer WHERE sess_id='".session_id()."' AND test_id=$tid";
+	$result3 = mysql_query($query3, $cn) or die(mysql_error());
+	$rowCount = mysql_num_rows($result3);
+	mysql_data_seek($result3, $_SESSION["qn"]);
+	$row = mysql_fetch_row($result3); 
 	
-}
-else
-{	
-		if (!isset($ans) and ($submit=='Next Question' or $submit=='Get Result')) {
-			echo "Please Select valid answer";
-		} 
-		else if($submit=='Next Question')
-		{
-				mysql_data_seek($rs,$_SESSION["qn"]);
-				$row= mysql_fetch_row($rs);	
-				mysql_query("insert into mst_useranswer(sess_id, test_id, que_des, ans1,ans2,ans3,ans4,true_ans,your_ans) values ('".session_id()."', $tid,'$row[2]','$row[3]','$row[4]','$row[5]', '$row[6]','$row[7]','$ans')") or die(mysql_error());
-				if($ans==$row[7])
-				{
-							$_SESSION["trueans"]=$_SESSION["trueans"]+1;
+	if($_SESSION["qn"] > $rowCount-1) {
+		unset($_SESSION["qn"]);
+		echo "<h1 class=head1>Some Error  Occured</h1>";
+		session_destroy();
+		echo "Please <a href=index.php> Start Again</a>";
+		exit;
+	} else {
+		if ($submit=='Next Question' or $submit=='Get Result') {
+			if (!isset($ans)) {
+				echo "Please Select valid answer";
+			} else {
+				$query4 = "UPDATE mst_useranswer SET your_ans=$ans";
+				mysql_query($query4, $cn) or die(mysql_error());
+			
+				if($ans == $row[7]) {
+					$_SESSION["trueans"] = $_SESSION["trueans"]+1;
 				}
-				$_SESSION["qn"]=$_SESSION["qn"]+1;
-		}
-		else if($submit=='Get Result')
-		{
-				mysql_data_seek($rs,$_SESSION["qn"]);
-				$row= mysql_fetch_row($rs);	
-				mysql_query("insert into mst_useranswer(sess_id, test_id, que_des, ans1,ans2,ans3,ans4,true_ans,your_ans) values ('".session_id()."', $tid,'$row[2]','$row[3]','$row[4]','$row[5]', '$row[6]','$row[7]','$ans')") or die(mysql_error());
-				if($ans==$row[7])
-				{
-							$_SESSION["trueans"]=$_SESSION["trueans"]+1;
+				$_SESSION["qn"] = $_SESSION["qn"]+1;
+			
+				if($submit=='Get Result') {
+					echo "<h1 class=head1> Result</h1>";
+					$percentage=($_SESSION["trueans"]/$_SESSION["qn"])*100;
+					echo $percentage."%. ".$_SESSION["trueans"]." out of ".$_SESSION["qn"].".";
+					mysql_query("insert into mst_result(login,test_id,test_date,score) values('$login',$tid,now(),'$percentage')") or die(mysql_error());
+					//echo "<h1 align=center><a href=review.php> Review Question</a> </h1>";
+					unset($_SESSION["qn"]);
+					unset($_SESSION["sid"]);
+					unset($_SESSION["tid"]);
+					unset($_SESSION["trueans"]);
+					session_destroy();
+					echo "Please <a href=index.php> Start Again</a>";
+					exit;
 				}
-				echo "<h1 class=head1> Result</h1>";
-				$_SESSION["qn"]=$_SESSION["qn"]+1;
-				echo "<Table align=center><tr class=tot><td>Total Question<td> $_SESSION[qn]";
-				echo "<tr class=tans><td>True Answer<td>".$_SESSION["trueans"];
-				$w=$_SESSION["qn"]-$_SESSION["trueans"];
-				echo "<tr class=fans><td>Wrong Answer<td> ". $w;
-				echo "</table>";
-				mysql_query("insert into mst_result(login,test_id,test_date,score) values('$login',$tid,'".date("d/m/Y")."',$_SESSION[trueans])") or die(mysql_error());
-				echo "<h1 align=center><a href=review.php> Review Question</a> </h1>";
-				unset($_SESSION["qn"]);
-				unset($_SESSION["sid"]);
-				unset($_SESSION["tid"]);
-				unset($_SESSION["trueans"]);
-				exit;
+	
+			}
 		}
+		
+		mysql_data_seek($result3, $_SESSION["qn"]);
+		$row = mysql_fetch_row($result3);
+	
+		renderData($row, $rowCount);
+	}
+} else {
+	unset($_SESSION["qn"]);
+	echo "<h1 class=head1>Some Error  Occured</h1>";
+	session_destroy();
+	echo "Please <a href=index.php> Start Again</a>";
+	exit;
 }
 
-if($_SESSION["qn"]>mysql_num_rows($rs)-1)
-{
-unset($_SESSION["qn"]);
-echo "<h1 class=head1>Some Error  Occured</h1>";
-session_destroy();
-echo "Please <a href=index.php> Start Again</a>";
+function renderData($row, $count) {
+	echo "<form name=myfm method=post action=quiz.php>";
+	echo "<table width=100%>";
+	echo "<tr><td width=30>&nbsp</td></tr>";
+	echo "<table border=0>";
+	$n=$_SESSION["qn"]+1;
+	echo "<tr><td><span class=style2>Que ".  $n .": $row[2]</span></td></tr>";
+	echo "<tr><td class=style8><input type=radio name=ans value=1>$row[3]</td></tr>";
+	echo "<tr><td class=style8> <input type=radio name=ans value=2>$row[4]</td></tr>";
+	echo "<tr><td class=style8><input type=radio name=ans value=3>$row[5]</td></tr>";
+	echo "<tr><td class=style8><input type=radio name=ans value=4>$row[6]</td></tr>";
 
-exit;
+	if($_SESSION["qn"] < $count-1)
+		echo "<tr><td><input type=submit name=submit value='Next Question'></td></tr>";
+	else
+		echo "<tr><td><input type=submit name=submit value='Get Result'></td></tr>";
+		
+	echo "</table></table></form>";
 }
-mysql_data_seek($rs,$_SESSION["qn"]);
-$row= mysql_fetch_row($rs);
-echo "<form name=myfm method=post action=quiz.php>";
-echo "<table width=100%> <tr> <td width=30>&nbsp;<td> <table border=0>";
-$n=$_SESSION["qn"]+1;
-echo "<tR><td><span class=style2>Que ".  $n .": $row[2]</style>";
-echo "<tr><td class=style8><input type=radio name=ans value=1>$row[3]";
-echo "<tr><td class=style8> <input type=radio name=ans value=2>$row[4]";
-echo "<tr><td class=style8><input type=radio name=ans value=3>$row[5]";
-echo "<tr><td class=style8><input type=radio name=ans value=4>$row[6]";
-
-if($_SESSION["qn"]<mysql_num_rows($rs)-1)
-echo "<tr><td><input type=submit name=submit value='Next Question'></form>";
-else
-echo "<tr><td><input type=submit name=submit value='Get Result'></form>";
-echo "</table></table>";
-
 ?>
 </body>
 </html>
